3a4c13a63d21243ce5a874dac7d280550da323ab
Carlos Favarão <carlosfavarao12@gmail.com>
2025-05-14 17:25:19 -0300

    Update FilmeModel.java

--- cinemaAutoatendimento/src/main/java/com/example/cinemaAutoatendimento/model/FilmeModel.java
+++ cinemaAutoatendimento/src/main/java/com/example/cinemaAutoatendimento/model/FilmeModel.java
@@ -1,4 +1,36 @@
 package com.example.cinemaAutoatendimento.model;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+@Entity
+@Table(name = "filme")
+@Getter
 public class FilmeModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Setter
+    @Column(nullable = false)
+    private String nome;
+
+    @Setter
+    @Column(nullable = false)
+    private LocalDate data_inicio;
+
+    @Setter
+    @Column(nullable = false)
+    private LocalDate data_fim;
+
+    public FilmeModel() {} //Construtor padrão, não lembrava dele porém, é essencial pra o funcionamento do JPA
+
+    public FilmeModel(LocalDate data_fim, String nome, LocalDate data_inicio) {
+        this.data_fim = data_fim;
+        this.nome = nome;
+        this.data_inicio = data_inicio;
+    }
 }
