65d6b45a93c1469d3da73c3e47c3d8563d41165f
Carlos Favarão <carlosfavarao12@gmail.com>
2025-05-19 14:17:26 -0300

    Adicionei os métodos em Pessoa Service

--- cinemaAutoatendimento/src/main/java/com/example/cinemaAutoatendimento/service/PessoaService.java
+++ cinemaAutoatendimento/src/main/java/com/example/cinemaAutoatendimento/service/PessoaService.java
@@ -1,8 +1,35 @@
 package com.example.cinemaAutoatendimento.service;
 
+import com.example.cinemaAutoatendimento.model.PessoaModel;
+import com.example.cinemaAutoatendimento.repository.PessoaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PessoaService {
+    @Autowired
+    private PessoaRepository pessoaRepository;
 
+    public PessoaModel salvarPessoa(PessoaModel pessoa){
+        return pessoaRepository.save(pessoa);
+    }
+
+    public List<PessoaModel> listarPessoas(){
+        return pessoaRepository.findAll();
+    }
+
+    public Optional<PessoaModel> buscarPessoaPorId(int id){
+        return pessoaRepository.findById(id);
+    }
+
+    public boolean excluirPessoa(int id) {
+        if (pessoaRepository.existsById(id)){
+            pessoaRepository.deleteById(id);
+            return true;
+        }
+        return false;
+    }
 }
