c585333d42542001146bfc66cf9c63efd16e07c0
Carlos Favarão <carlosfavarao12@gmail.com>
2025-05-22 14:31:59 -0300

    Update FilmeRepository.java

--- cinemaAutoatendimento/src/main/java/com/example/cinemaAutoatendimento/repository/FilmeRepository.java
+++ cinemaAutoatendimento/src/main/java/com/example/cinemaAutoatendimento/repository/FilmeRepository.java
@@ -10,5 +10,5 @@ import java.util.List;
 @Repository
 public interface FilmeRepository extends JpaRepository<FilmeModel, Long> {
     @Query("SELECT f FROM FilmeModel f WHERE f.dataInicio <= CURRENT_DATE AND f.dataFim >= CURRENT_DATE")
-    List<FilmeModel> findFilmesEmCartazHoje();
-}
+    List<FilmeModel> findFilmesEmCartazHoje(); //Faz com que o BD faça a busca, minimizando o gasto de memória
+}                                              //Tira a necessidade de baixar todo o BD para realizar o filtro     
